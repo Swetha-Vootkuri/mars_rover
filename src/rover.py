eb451ba55c462e7e07c9bd9b4fac0394f93c486c
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-08-11 16:13:03 +0100

    Added the src folder to the project

---
+++ src/rover.py
@@ -0,0 +1,7 @@
+class Rover:
+    def __init__(self, position_x, position_y):
+        self.position_x = position_x
+        self.position_y = position_y
+
+    def get_position(self):
+        return self.position_x, self.position_y
