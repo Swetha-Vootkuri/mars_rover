95d2b5da2ba43870164048334d2aa0d65bfe49dd
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-08-17 16:00:24 +0100

    removed redundant test

--- test/test_rover_move_forward.py
+++ test/test_rover_move_forward.py
@@ -9,21 +9,6 @@ from src.rover import Rover
 
 class RoverMoveForward(unittest.TestCase):
 
-    def test_move_forward_facing_north(self):
-        rover = Rover(1, 1, Direction.NORTH)
-        rover.receive_command("F")
-        self.assertEqual((1, 2), rover.get_position())
-
-    def test_move_forward_facing_east(self):
-        rover = Rover(1, 1, Direction.EAST)
-        rover.receive_command("F")
-        self.assertEqual((2, 1), rover.get_position())
-
-    def test_move_forward_facing_south(self):
-        rover = Rover(1, 1, Direction.SOUTH)
-        rover.receive_command("F")
-        self.assertEqual((1, 0), rover.get_position())
-
     @parameterized.expand([(Direction.NORTH, (1, 2)),
                            (Direction.EAST, (2, 1)),
                            (Direction.SOUTH, (1, 0))])
