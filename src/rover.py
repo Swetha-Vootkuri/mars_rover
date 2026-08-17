b0a059cfa34e0d93ecb1a607c99d9cc02bb3744d
Vootkuri, Swetha (Automation Developer - Sky) <swetha.vootkuri+comcast@sky.uk>
2026-08-17 14:32:57 +0100

    Added test for initialise rover heading

--- src/rover.py
+++ src/rover.py
@@ -1,7 +1,11 @@
 class Rover:
-    def __init__(self, position_x, position_y):
+    def __init__(self, position_x, position_y, heading):
+        self.heading = heading
         self.position_x = position_x
         self.position_y = position_y
 
     def get_position(self):
         return self.position_x, self.position_y
+
+    def get_heading(self):
+        return self.heading
--- test/test_initialise_rover.py
+++ test/test_initialise_rover.py
@@ -1,10 +1,20 @@
 import unittest
+from enum import Enum
 
 from src.rover import Rover
+
+
+class Direction(Enum):
+    NORTH = 1
 
 
 class InitialiseRoverTest(unittest.TestCase):
 
     def test_initialise_rover(self):
-        rover = Rover(2, 3)
+        rover = Rover(2, 3, Direction.NORTH)
         self.assertEqual((2, 3), rover.get_position())
+
+    def test_initialise_rover_heading(self):
+        rover = Rover(2, 3, Direction.NORTH)
+        self.assertEqual(Direction.NORTH,rover.get_heading())
+
