36fcaa070d76d1f96a9cf5cf7ad59ab808c51fda
Praveena Ramachandran <praveena.ramachandran@sky.uk>
2026-08-17 15:59:09 +0100

    parameterised the test to simply the test code

--- test/test_rover_move_forward.py
+++ test/test_rover_move_forward.py
@@ -1,6 +1,8 @@
 import unittest
 
-from src import rover
+from parameterized import parameterized
+import parametrize
+
 from src.direction import Direction
 from src.rover import Rover
 
@@ -21,3 +23,11 @@ class RoverMoveForward(unittest.TestCase):
         rover = Rover(1, 1, Direction.SOUTH)
         rover.receive_command("F")
         self.assertEqual((1, 0), rover.get_position())
+
+    @parameterized.expand([(Direction.NORTH, (1, 2)),
+                           (Direction.EAST, (2, 1)),
+                           (Direction.SOUTH, (1, 0))])
+    def test_move_forward(self, direction, expected_final_position):
+        rover = Rover(1, 1, direction)
+        rover.receive_command("F")
+        self.assertEqual(expected_final_position, rover.get_position())
